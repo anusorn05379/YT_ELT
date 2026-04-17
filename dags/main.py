eff83a0d5187aaa3f00a45d80c082bab8f70e5fb
anusron lueaklang <kiwanusorn05379@gmail.com>
2026-04-17 22:01:37 +0700

    Add Github Action YAML woreflow file. Changer docker compose to on longer ues the .env and use version 8.3.4 of pytest

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 import pendulum
-from datetime import datetime ,timedelta
+from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 from api.video_stats import (
     get_playlist_id,
     get_video_ids,
@@ -11,61 +13,79 @@ from api.video_stats import (
 from datawarehoues.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
 
-local_tz = pendulum.timezone("Asia/Bangkok")
+# Define the local timezone
+local_tz = pendulum.timezone("Europe/Malta")
 
+# Default Args
 default_args = {
     "owner": "dataengineers",
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "email": "dataengineers@example.com",
-    # "retries": 1,
-    # "retry_delay": timedelta(minutes=5),
+    "email": "data@engineers.com",
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
     "max_active_runs": 1,
     "dagrun_timeout": timedelta(hours=1),
-    "start_date": datetime(2026, 4, 17, tzinfo=local_tz),
-    # "end_date": datetime(2026, 4, 30, tzinfo=local_tz)
+    "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
+    # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
-
 
 # Variables
 staging_schema = "staging"
 core_schema = "core"
 
-
+# DAG 1: produce_json
 with DAG(
     dag_id="produce_json",
     default_args=default_args,
-    description="A DAG to extract video data from YouTube API and save it as JSON",
-    schedule='0 14 * * *',  # Run every day at 14:00 ICT (UTC+7)
-    catchup=False
-)as dag:
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False,
+) as dag_produce:
 
+    # Define tasks
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    video_data = extract_video_data(video_ids)
-    save_to_json(video_data)
+    extract_data = extract_video_data(video_ids)
+    save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
+    # Define dependencies
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
+
+# DAG 2: update_db
 with DAG(
     dag_id="update_db",
     default_args=default_args,
-    description="DAG to process JSON files and update the staging and core schemas",
-    schedule='0 15 * * *',  # Run every day at 15:00 ICT (UTC+7)
-    catchup=False
-)as dag:
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
 
+    # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
-    update_staging >> update_core
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
+    # Define dependencies
+    update_staging >> update_core >> trigger_data_quality
 
+# DAG 3: data_quality
 with DAG(
     dag_id="data_quality",
     default_args=default_args,
     description="DAG to check the data quality on both layers in the database",
     catchup=False,
-    schedule='0 16 * * *',  # Run every day at 16:00 ICT (UTC+7)
+    schedule=None,
 ) as dag_quality:
 
     # Define tasks
